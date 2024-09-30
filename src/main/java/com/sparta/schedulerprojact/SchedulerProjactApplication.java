fa0cda6ed4252f0cc5297479c97b6667f4ffe7d5
jubseok <junseok708@gmail.com>
2024-09-30 11:13:52 +0900

    first commit

---
+++ src/main/java/com/sparta/schedulerprojact/SchedulerProjactApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.schedulerprojact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerProjactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerProjactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/schedulerprojact/SchedulerProjactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.schedulerprojact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerProjactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
